a2f054df4ad7d5bd17e98493dadf68d9efae749f
Hevdarus <zsoltdede712@gmail.com>
2022-11-27 12:28:11 +0100

    Add email sender

--- reservation.php
+++ reservation.php
@@ -1,50 +1,52 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Asztalfoglalás</title>
   <link rel="stylesheet" type="text/css" href="style.css">
   <?php
-  echo '<link rel="stylesheet" type="text/css" href="style.css?'.filemtime('style.css').'">';
+  echo '<link rel="stylesheet" type="text/css" href="style.css?' . filemtime('style.css') . '">';
 
   ?>
 
 
 </head>
+
 <body>
   <div class="header">
     <h2>Asztalfoglalás</h2>
   </div>
-  <?php  if (!isset($_SESSION['success'])) : ?>
-    <form method="post" action="reservation.php">
-      <?php include('errors.php'); ?>
-      <div class="input-group">
-        <label style="display:inline; margin-right:20px">Asztal mérete</label>
-        <select class="btn" name="people">
-            <option value=2>2 személyes</option>
-            <option value=4>4 személyes</option>
-            <option value=6>6 személyes</option>
-            <option value=8>8 személyes</option>
-            <option value=10>10 személyes</option>
-            <option value=12>12 személyes</option>
-            <option value=16>16 személyes</option>
-            <option value=20>20 személyes</option>
-            <option value=30>30 személyes</option>
-          </select>
-      </div>
-      <div class="input-group">
-        <label>Dátum</label>
-        <input type="datetime-local" name="date"> 
-      </div>
-      <div class="input-group">
-        <button type="submit" class="btn" name="table_reservation">Foglalás!</button>
-      </div>
-    </form>  
-  <?php endif ?>
-  <?php  if (isset($_SESSION['success'])) : ?>
-    <div class="content">
-      <div class="success">Sikeres Asztalfoglalás! </div>
+  <?php if (!isset($_SESSION['success'])): ?>
+  <form method="post" action="reservation.php">
+    <?php include('errors.php'); ?>
+    <div class="input-group">
+      <label style="display:inline; margin-right:20px">Asztal mérete</label>
+      <select class="btn" name="people">
+        <option value=2>2 személyes</option>
+        <option value=4>4 személyes</option>
+        <option value=6>6 személyes</option>
+        <option value=8>8 személyes</option>
+        <option value=10>10 személyes</option>
+        <option value=12>12 személyes</option>
+        <option value=16>16 személyes</option>
+        <option value=20>20 személyes</option>
+        <option value=30>30 személyes</option>
+      </select>
     </div>
+    <div class="input-group">
+      <label>Dátum</label>
+      <input type="datetime-local" name="date">
+    </div>
+    <div class="input-group">
+      <button type="submit" class="btn" name="table_reservation">Foglalás!</button>
+    </div>
+  </form>
+  <?php endif ?>
+  <?php if (isset($_SESSION['success'])): ?>
+  <div class="content">
+    <div class="success">Sikeres Asztalfoglalás! </div>
+  </div>
   <?php
     echo '<script type="text/JavaScript"> 
                       function navigate() {
@@ -55,8 +57,9 @@
     unset($_SESSION['success']);
   ?>
   <?php endif ?>
-  
 
-  
+
+
 </body>
+
 </html>
--- server.php
+++ server.php
@@ -2,11 +2,11 @@
 session_start();
 
 $username = "";
-$email    = "";
-$errors = array(); 
+$email = "";
+$errors = array();
 
 
-$db = mysqli_connect('localhost', 'root', '', 'RestaurantProject');  
+$db = mysqli_connect('localhost', 'root', '', 'RestaurantProject');
 
 if (isset($_POST['reg_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
@@ -14,18 +14,24 @@ if (isset($_POST['reg_user'])) {
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  if (empty($username)) { array_push($errors, "Adja meg a felhasznólénevet"); }
-  if (empty($email)) { array_push($errors, "Adja meg az email címet"); }
-  if (empty($password_1)) { array_push($errors, "Adja meg a jelszót"); }
+  if (empty($username)) {
+    array_push($errors, "Adja meg a felhasznólénevet");
+  }
+  if (empty($email)) {
+    array_push($errors, "Adja meg az email címet");
+  }
+  if (empty($password_1)) {
+    array_push($errors, "Adja meg a jelszót");
+  }
   if ($password_1 != $password_2) {
-	array_push($errors, "A két jelszó nem egyezik meg");
+    array_push($errors, "A két jelszó nem egyezik meg");
   }
 
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
-  
-  if ($user) { 
+
+  if ($user) {
     if ($user['username'] === $username) {
       array_push($errors, "A felhasználónév már létezik");
     }
@@ -37,11 +43,11 @@ if (isset($_POST['reg_user'])) {
 
 
   if (count($errors) == 0) {
-  	$query = "INSERT INTO users (username, email, password) 
+    $query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password_1')";
-  	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	header('location: index.php');
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    header('location: index.php');
   }
 }
 
@@ -50,21 +56,21 @@ if (isset($_POST['login_user'])) {
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
   if (empty($username)) {
-  	array_push($errors, "Adja meg a felhasználónevét");
+    array_push($errors, "Adja meg a felhasználónevét");
   }
   if (empty($password)) {
-  	array_push($errors, "Adja meg a jelszavát");
+    array_push($errors, "Adja meg a jelszavát");
   }
 
   if (count($errors) == 0) {
-  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-  	$results = mysqli_query($db, $query);
-  	if (mysqli_num_rows($results) == 1) {
-  	  $_SESSION['username'] = $username;
-  	  header('location: index.php');
-  	}else {
-  		array_push($errors, "Rossz felhasználónév/jelszó");
-  	}
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $results = mysqli_query($db, $query);
+    if (mysqli_num_rows($results) == 1) {
+      $_SESSION['username'] = $username;
+      header('location: index.php');
+    } else {
+      array_push($errors, "Rossz felhasználónév/jelszó");
+    }
   }
 }
 
@@ -77,15 +83,23 @@ if (isset($_POST['table_reservation'])) {
   $user = mysqli_fetch_assoc($result);
   $id = $user['id'];
 
-  if (empty($date)) { array_push($errors, "Adjon meg egy dátumot"); }
-  if (empty($people)) { array_push($errors, "Adja meg a vendégek számát"); }
-  if (empty($id)) { array_push($errors, "Jelentkezzen be"); }
-  if (time()+(60*60*12) > strtotime($date)) { array_push($errors, "Asztalfoglalást minimum 12 órával előre szükséges leadni"); }
+  if (empty($date)) {
+    array_push($errors, "Adjon meg egy dátumot");
+  }
+  if (empty($people)) {
+    array_push($errors, "Adja meg a vendégek számát");
+  }
+  if (empty($id)) {
+    array_push($errors, "Jelentkezzen be");
+  }
+  if (time() + (60 * 60 * 12) > strtotime($date)) {
+    array_push($errors, "Asztalfoglalást minimum 12 órával előre szükséges leadni");
+  }
 
   if (count($errors) == 0) {
-  	$query = "INSERT INTO table_reservation (user_id, people, tr_date) 
+    $query = "INSERT INTO table_reservation (user_id, people, tr_date) 
   			  VALUES('$id', '$people', '$date')";
-  	mysqli_query($db, $query);
+    mysqli_query($db, $query);
     $_SESSION['success'] = "1";
   }
 }
@@ -96,7 +110,7 @@ if (isset($_POST['order_food'])) {
   $date = $_POST['date'];
   //if (empty($rendeles)) {array_push($errors, "Tegyen termékeket a rendelésbe");}
   if (isset($_POST['rendeles'])) {
-    $rendeles = explode(',',$_POST['rendeles']);
+    $rendeles = explode(',', $_POST['rendeles']);
   }
   $username = $_SESSION['username'];
   $get_id_query = "SELECT id FROM users WHERE username='$username'";
@@ -104,38 +118,74 @@ if (isset($_POST['order_food'])) {
   $user = mysqli_fetch_assoc($result);
   $id = $user['id'];
 
-  if (empty($date)) { array_push($errors, "Adjon meg egy dátumot"); }
-  if (empty($phone)) { array_push($errors, "Adja meg a telefonszámát"); }
-  if (empty($address)) { array_push($errors, "Adja meg a címét"); }
-  if (empty($id)) { array_push($errors, "Jelentkezzen be"); }
-  if (empty($_POST['rendeles'])) {array_push($errors, "Tegyen termékeket a rendelésbe");}
-  if (time()+(60*60) > strtotime($date)) { array_push($errors, "Rendelést minimum 1 órával előre szükséges leadni"); }
+  if (empty($date)) {
+    array_push($errors, "Adjon meg egy dátumot");
+  }
+  if (empty($phone)) {
+    array_push($errors, "Adja meg a telefonszámát");
+  }
+  if (empty($address)) {
+    array_push($errors, "Adja meg a címét");
+  }
+  if (empty($id)) {
+    array_push($errors, "Jelentkezzen be");
+  }
+  if (empty($_POST['rendeles'])) {
+    array_push($errors, "Tegyen termékeket a rendelésbe");
+  }
+  if (time() + (60 * 60) > strtotime($date)) {
+    array_push($errors, "Rendelést minimum 1 órával előre szükséges leadni");
+  }
 
   if (count($errors) == 0) {
-  	$query = "INSERT INTO orders (user_id, o_address, phone, o_date) 
+    $query = "INSERT INTO orders (user_id, o_address, phone, o_date) 
   			  VALUES('$id', '$address', '$phone', '$date')";
-  	mysqli_query($db, $query);
-/*
+    mysqli_query($db, $query);
+    /*
     $get_id_query = "SELECT id FROM orders ORDER BY id DESC LIMIT 1";
     $result = mysqli_query($db, $get_id_query);
     $fetch = mysqli_fetch_assoc($result);
     $id2 = $fetch['id'];
-*/
+    */
     $id2 = mysqli_insert_id($db);
-    for($n = 0; $n<count($rendeles); $n += 3) {
+    for ($n = 0; $n < count($rendeles); $n += 3) {
       $f_name = strval($rendeles[$n]);
-      $price = strval($rendeles[$n+1]);
-      $amount = intval($rendeles[$n+2]);
+      $price = strval($rendeles[$n + 1]);
+      $amount = intval($rendeles[$n + 2]);
 
       $query2 = "INSERT INTO ordered_food (order_id, f_name, price, amount) 
   			  VALUES('$id2', '$f_name', '$price', '$amount')";
-  	  mysqli_query($db, $query2);
+      mysqli_query($db, $query2);
     }
 
-  	$_SESSION['success'] = "1";
+    $_SESSION['success'] = "1";
   }
-  
+
 }
 
+if (isset($_POST['email_sender'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
 
+
+  if (empty($name)) {
+    array_push($errors, "Adja meg a nevét!");
+  }
+  if (empty($email)) {
+    array_push($errors, "Adja meg az emailcímét!");
+  }
+  if (empty($message)) {
+    array_push($errors, "Írjon üzenetet!");
+  }
+
+  if (count($errors) == 0) {
+    $query = "INSERT INTO email (name, email, message)
+  VALUES('$name', '$email', '$message')";
+    mysqli_query($db, $query);
+
+    $_SESSION['success'] = "1";
+  }
+
+}
 ?>
